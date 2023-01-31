8356c6a6593b9ed54fc80a910770fce3e35d36d2
Mika Braginsky <mika.br@gmail.com>
2023-01-31 18:15:31 -0500

    shorten evalue example

--- inst/examples/multibias_evalue.R
+++ inst/examples/multibias_evalue.R
@@ -1,8 +1,3 @@
-multibias_evalue(yi = meta_meat$yi,
-                 vi = meta_meat$vi,
-                 biased = !meta_meat$randomized,
-                 selection_ratio = 4)
-
 # specify confounding as internal bias
 multibias_evalue(yi = meta_meat$yi,
                  vi = meta_meat$vi,
