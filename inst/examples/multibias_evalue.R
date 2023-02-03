af197112f79388f47c52059f3688311a40417947
Mika Braginsky <mika.br@gmail.com>
2023-02-03 14:45:34 -0500

    wrap evalue examples in donttest

--- inst/examples/multibias_evalue.R
+++ inst/examples/multibias_evalue.R
@@ -1,14 +1,16 @@
-# specify confounding as internal bias
-evalues <- multibias_evalue(yi = meta_meat$yi,
-                            vi = meta_meat$vi,
-                            biased = !meta_meat$randomized,
-                            selection_ratio = 4)
-evalues$stats
+\donttest{
+  # specify confounding as internal bias
+  evalues <- multibias_evalue(yi = meta_meat$yi,
+                              vi = meta_meat$vi,
+                              biased = !meta_meat$randomized,
+                              selection_ratio = 4)
+  evalues$stats
 
-# specify confounding as internal bias
-evalues_confounding <- multibias_evalue(yi = meta_meat$yi,
-                                        vi = meta_meat$vi,
-                                        biased = !meta_meat$randomized,
-                                        selection_ratio = 4,
-                                        assumed_bias_type = list(EValue::confounding()))
-evalues_confounding$stats
+  # specify confounding as internal bias
+  evalues_confounding <- multibias_evalue(yi = meta_meat$yi,
+                                          vi = meta_meat$vi,
+                                          biased = !meta_meat$randomized,
+                                          selection_ratio = 4,
+                                          assumed_bias_type = list(EValue::confounding()))
+  evalues_confounding$stats
+}
